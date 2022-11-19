2aa43847052dee2593b5f974a9f0d33f70961e4c
HollywoodOKC <T_Lane11@yahoo.com>
2022-11-19 14:44:24 -0600

    Initial Commit

---
+++ gitInfo.js
@@ -0,0 +1,77 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.
+    You'll provide the command as well as what it does.
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+const gitDefinition = `Is the technology that allows us to backup various stages of our code.`;
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+const gitHubDefinition = `The central location where those back-ups can be stored and updated online in case something happens to the computer or just to share.`;
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+const gitInitDefinition = `The way to initialize a git repo.`;
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+const gitCloneDefinition = `This command clones the selected repo and makes it ready for a new location.`;
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+const gitStatusDefinition = `Shows which files have been modified and which ones are ready to be committed.`;
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.
+    It should be a string containing the code to add all files.
+*/
+
+const gitAddDefinition = `Adds whatever files that were changed to the area to be committed.`;
+const gitAddCode = `git add -p`;
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+const gitCommitDefinition = `This command makes the commit or snapshot of the code exactly the way it is at the time of the commit.`;
+const gitCommitCode = `git commit -m "initial commit"`;
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+const gitPushDefinition = `This command sends the commit to your remote repo`;
