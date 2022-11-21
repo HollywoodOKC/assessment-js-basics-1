4aed44dcb0fbf37933eb0128ab25f935c89cc1a3
HollywoodOKC <T_Lane11@yahoo.com>
2022-11-20 19:46:46 -0600

    Attempt.

--- orchard.js
+++ orchard.js
@@ -3,35 +3,35 @@
 ///////////////////
 
 /*
-    The data below has been collected over the 
-    last week at an apple orchard. You will be 
-    using your coding skills to gain insights 
+    The data below has been collected over the
+    last week at an apple orchard. You will be
+    using your coding skills to gain insights
     into its daily operations.
 
     Below are three arrays each containing 7
     numbers. You will use them and the price
-    variables to complete the problems in 
+    variables to complete the problems in
     this file.
 
-    The indexes of the arrays correspond with a 
-    day of the week. The associated number value 
-    represents the number of acres that were picked 
+    The indexes of the arrays correspond with a
+    day of the week. The associated number value
+    represents the number of acres that were picked
     that day.
 
-    The first array is named fujiAcres. Notice 
-    that the first item in this array is 2, 
-    meaning that 2 acres of Fuji apples were 
+    The first array is named fujiAcres. Notice
+    that the first item in this array is 2,
+    meaning that 2 acres of Fuji apples were
     picked on Monday of this week.
 
     The prices are per pound and are written
-    in cents. 
+    in cents.
 */
 
 const fujiAcres = [2, 3, 3, 2, 2, 2, 1]
 const galaAcres = [5, 2, 4, 3, 6, 2, 4]
 const pinkAcres = [1, 5, 4, 2, 1, 5, 4]
 
-const fujiPrice = .89 
+const fujiPrice = .89
 const galaPrice = .64
 const pinkPrice = .55
 
@@ -43,26 +43,29 @@ const pinkPrice = .55
     Using for loop, calculate the total number
     of acres picked for the entire week.
 
-    Save the number to a variable called 
+    Save the number to a variable called
     `totalAcres`.
 
     Log `totalAcres` to the console.
 */
 
-// CODE HERE
+let totalAcres = new Array();
 
-
-
+for (let s = 0; s < fujiAcres.length; s++) {
+    totalAcres.push(fujiAcres[s] + galaAcres[s] + pinkAcres[s]);
+    console.log(totalAcres);
+}
+console.log(totalAcres);
 
 
 // PROBLEM 2
 
 /*
-    Using `totalAcres`, calculate the average 
-    number of acres picked per day. 
+    Using `totalAcres`, calculate the average
+    number of acres picked per day.
 
-    Save the answer to a variable called 
-    `averageDailyAcres`. 
+    Save the answer to a variable called
+    `averageDailyAcres`.
 
     Log `averageDailyAcres` to the console.
 */
@@ -76,19 +79,19 @@ const pinkPrice = .55
 // PROBLEM 3
 
 /*
-    We have provided 2 variables below. 
-    
-    The `acresLeft` variable is the number 
-    of acres that still have apples left. 
-    
-    The `days` variable represents how 
-    many more days of work are left. It's 
-    initialized at 0 because we're going 
+    We have provided 2 variables below.
+
+    The `acresLeft` variable is the number
+    of acres that still have apples left.
+
+    The `days` variable represents how
+    many more days of work are left. It's
+    initialized at 0 because we're going
     to be using it as a counter.
-    
+
     Write a while loop that will continue to run
-    while `acresLeft` is above 0. 
-    
+    while `acresLeft` is above 0.
+
     On each iteration of the loop:
     - add 1 to the `days` variable
     - subtract your daily average from the number of acres left
@@ -97,12 +100,12 @@ const pinkPrice = .55
 
     Note: This is not the most efficient way to
     calculate this number. But! It is a great
-    way to get a whole number without using 
-    any Math methods. 
+    way to get a whole number without using
+    any Math methods.
 
 */
 
-let acresLeft = 174 
+let acresLeft = 174
 let days = 0
 
 // CODE HERE
@@ -113,23 +116,23 @@ let days = 0
 
 /*
     Your next task is to create 3 arrays
-    that list the daily amount of apples 
-    picked, in tons, for each variety. 
-    
+    that list the daily amount of apples
+    picked, in tons, for each variety.
+
     Each acre yields 6.5 tons of apples.
 
-    Use the variables below to store 
+    Use the variables below to store
     your new arrays. Make sure that you
-    don't modify the original arrays 
+    don't modify the original arrays
     on lines 36 - 38.
 
     Log each of your arrays to the console.
 
-    Hint: you can use the slice method 
-    to make copies of the arrays and 
+    Hint: you can use the slice method
+    to make copies of the arrays and
     then loop the copies. You could also
     make empty arrays, loop the old ones
-    and use the push method to add new 
+    and use the push method to add new
     values to the new arrays.
 */
 
@@ -147,20 +150,20 @@ let days = 0
 // PROBLEM 5
 
 /*
-    Next, calculate the total number of 
+    Next, calculate the total number of
     pounds picked per variety.
 
     You'll need to add up the tons per
-    each variety and convert the number 
+    each variety and convert the number
     into pounds -- store that number in
-    the variables given below. 
+    the variables given below.
 
     Log each of the values to the console.
 
     Hint: there are 2000 pounds in a ton.
 */
 
-// CODE HERE 
+// CODE HERE
 
 // let fujiPounds =
 // let galaPounds =
@@ -175,16 +178,16 @@ let days = 0
 
 /*
     Now that you know the total pounds
-    per variety, use the prices given 
-    at the beginning of this file to 
-    figure out how much you'll make 
-    from selling each type of apple. 
+    per variety, use the prices given
+    at the beginning of this file to
+    figure out how much you'll make
+    from selling each type of apple.
 
-    The prices are per pound and are 
-    written in cents. 
+    The prices are per pound and are
+    written in cents.
 
-    Log each of the profits to the 
-    console. 
+    Log each of the profits to the
+    console.
 */
 
 // CODE HERE
@@ -201,8 +204,8 @@ let days = 0
 // PROBLEM 7
 
 /*
-    Add up all your profits and save 
-    the number to a variable called 
+    Add up all your profits and save
+    the number to a variable called
     `totalProfit`.
 
     Log `totalProfit` to the console.
